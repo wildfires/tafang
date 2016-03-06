cd5bb2389a9fc011d57d632f773ee371dcd44808
高洋 <GYing@gaoyangdemacbook-pro.local>
2016-03-06 14:21:04 +0800

    Initial Commit

---
+++ tafang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tafang
+//
+//  Created by 高洋 on 16/3/6.
+//  Copyright © 2016年 Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
